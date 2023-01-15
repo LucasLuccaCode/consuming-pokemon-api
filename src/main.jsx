aabc1dc565dbda3c70d030086b0553d7287b431c
Lucca Santos <lucasluccad.r@outlook.com>
2023-01-15 17:22:55 -0300

    fix: disabled react RestricMode fixing the action of loading components twice when using useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import router from "./routes/index"
 import Global from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <RouterProvider router={router} />
     <Global />
-  </React.StrictMode>
+  </>
 )
